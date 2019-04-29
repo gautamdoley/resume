efd12534ea74fb25cdebadb998087eecd339ce98
gautamdoley <gautam.dole@gmail.com>
2019-04-29 11:43:35 +0530

    Updated resume

--- src/components/fullName.js
+++ src/components/fullName.js
@@ -5,7 +5,7 @@ class Name extends React.Component{
     return (
         <div>
         <h1>Gautam Doley</h1>
-        <h3 className="h5">Former Software Developer at Gratia Technology Pvt. Ltd., Guwahati</h3>
+        <h3 className="h5">Software Developer at Gratia Technology Pvt. Ltd., Guwahati</h3>
         </div>
       );
   }
