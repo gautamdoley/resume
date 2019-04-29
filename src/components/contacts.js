041269621327f34fe4fdbf4ee84709975d4e32cc
gautamdoley <gautam.dole@gmail.com>
2019-04-29 11:18:40 +0530

    Updated resume

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -6,7 +6,7 @@ class Contacts extends React.Component{
         <div>
         	<h3>Email:<span className="m-l">gautam.dole@gmail.com</span></h3>
         	<h3 className="m-t">Contact No:<span className="m-l">+916000061244</span></h3>
-        	<h3 className="m-t">Gihub Resume:<span className="m-l">https://gautamdoley.github.io/resume/</span></h3>
+        	<h3 className="m-t">Github Resume:<span className="m-l">https://gautamdoley.github.io/resume/</span></h3>
         	<h3 className="m-t">LinkedIn:<span className="m-l">https://www.linkedin.com/in/gautam-doley-651505173/</span></h3>
         </div>
       );
